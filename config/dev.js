4d0adb857bd830066be3cfa8d4a55ad68de7ac5f
Nitzan <prnitzan@gmail.com>
2023-08-04 22:57:10 +0300

    Add socket room creating

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
 export default  {
   dbURL: 'mongodb+srv://mainUser:ab123@cluster0.hmffezu.mongodb.net/',
+  // dbURL: 'mongodb://127.0.0.1:27017',
   dbName : 'Tuneify'
 }
--- services/socket.service.js
+++ services/socket.service.js
@@ -15,6 +15,8 @@ export function setupSocketAPI(http) {
             logger.info(`Socket disconnected [id: ${socket.id}]`)
         })
         socket.on('chat-set-topic', topic => {
+            console.log('entered chat-set-topic')
+            console.log('topic', socket.myTopic)
             if (socket.myTopic === topic) return
             if (socket.myTopic) {
                 socket.leave(socket.myTopic)
@@ -33,7 +35,6 @@ export function setupSocketAPI(http) {
         socket.on('user-watch', userId => {
             logger.info(`user-watch from socket [id: ${socket.id}], on user ${userId}`)
             socket.join('watching:' + userId)
-            
         })
         socket.on('set-user-socket', userId => {
             logger.info(`Setting socket.userId = ${userId} for socket [id: ${socket.id}]`)
@@ -65,11 +66,11 @@ async function emitToUser({ type, data, userId }) {
     }
 }
 
-// If possible, send to all sockets BUT not the current socket 
+// If possible, send to all sockets BUT not the current socket
 // Optionally, broadcast to a room / to all
 async function broadcast({ type, data, room = null, userId }) {
     userId = userId.toString()
-    
+
     logger.info(`Broadcasting event: ${type}`)
     const excludedSocket = await _getUserSocket(userId)
     if (room && excludedSocket) {
@@ -111,9 +112,9 @@ export const socketService = {
     // set up the sockets service and define the API
     setupSocketAPI,
     // emit to everyone / everyone in a specific room (label)
-    emitTo, 
+    emitTo,
     // emit to a specific user (if currently active in system)
-    emitToUser, 
+    emitToUser,
     // Send to all sockets BUT not the current socket - if found
     // (otherwise broadcast to a room / to all)
     broadcast,
