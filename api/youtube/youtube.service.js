dca68acae17b6cb562cb16a0d6a4717ae366273d
Yaron Levy <ron5054@gmail.com>
2023-08-27 13:21:26 +0300

    Add logs to track working api keys

--- api/youtube/youtube.service.js
+++ api/youtube/youtube.service.js
@@ -9,18 +9,19 @@ export const youtubeService = {
 let currApiKeyIdx = 0
 
 export async function getYoutubeId(term, retryCount = 0) {
-    if (retryCount > 5) return
+    if (retryCount >= (API_KEYS.length * 2)) return console.log('Tried all api keys unsuccessfully')
     const API_KEYS = process.env.API_KEYS.split(',')
 
     const currApiKey = API_KEYS[currApiKeyIdx]
     const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${currApiKey}&q=${term}`
     try {
         const response = await axios.get(url)
+        console.log(`Got response using api key number: ${currApiKeyIdx}`)
         return response.data
     } catch (error) {
         console.error('Error fetching data:', error)
+        console.log(`ERROR using api key ${currApiKeyIdx} - trying the next API key`)
         currApiKeyIdx = (currApiKeyIdx + 1) % API_KEYS.length
-        console.log('Switching to the next API key.')
         return getYoutubeId(term, retryCount + 1)
     }
 }
